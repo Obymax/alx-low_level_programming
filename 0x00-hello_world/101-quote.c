0152938fc08fa9f8bb8c46c9d444306df0389bcd
Obadia <kipz.obed1@gmail.com>
2022-09-08 17:52:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,3 @@
- /*
-  *  * File: 101-quote.c
-  *   * Auth: Brennan D Baraban
-  */
-
 #include <unistd.h>
 
 /**
@@ -14,8 +9,8 @@
 int main(void)
 {
 		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,
-					      59");
+					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+					      	      59);
 
 			return (1);
 }
