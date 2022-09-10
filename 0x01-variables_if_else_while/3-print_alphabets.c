44d45df81d4f9f8c8292a61f7627dc78de35150e
Obadia <kipz.obed1@gmail.com>
2022-09-09 17:33:22 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	    
+	    char ch;
+	   
+	    printf("Uppercase characters : \n");
+	  
+	    for (ch = 'A'; ch <= 'Z'; ch++)
+	    {
+	    printf("%c ", ch);
+	    }
+	 
+	    printf("\nLowercase characters : \n");
+	    for (ch = 'a'; ch <= 'z'; ch++)
+	    {
+	    printf("%c ", ch);
+	    }
+	    
+	    return 0;
+
