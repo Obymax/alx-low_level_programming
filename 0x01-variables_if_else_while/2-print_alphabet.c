bf3845b4123a45370c6f9d07baa5ef9e53c12859
Obadia <kipz.obed1@gmail.com>
2022-09-09 17:25:17 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,7 @@
 /* C Program to Print Alphabets from a to z */
 
-#include <stdio.h>
+#include <stdio.h> 
  
-int main(void)
-{
-	  	char ch;
-
-		printf("\n List of Alphabets from a to z are : \n");  
-		for(ch = 'a'; ch <= 'z'; ch++)
-		{
-		printf(" %c\t", ch);	
-		}
-				  
-		return 0;
+int main() { 
+	  putchar("abcdefghijkmnopqrstuvwxyz"); 
 }
