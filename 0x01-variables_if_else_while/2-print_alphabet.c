cf90a5e7ba08d5529b96cea5b2a5f685464fdda2
Obadia <kipz.obed1@gmail.com>
2022-09-11 11:14:38 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,18 @@
-/* C Program to Print Alphabets from a to z */
-
 #include <stdio.h>
-int main()
+/**
+ * * main - print if the number is postive, zero, or negative
+ * *
+ * * Description: using the main function
+ * * this program prints "Programming is positive, zero, or negative
+ * * Return: 0
+ */
+int main(void)
 {
-	int ch;
-	for(ch='a'; ch<='z'; ch++)
-	putchar(ch);
-
-
-
-
-
-
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
