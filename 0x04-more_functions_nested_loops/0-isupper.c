3f79340ad7e9e2e55294d8c8f055b424100b5e73
Obadia <kipz.obed1@gmail.com>
2022-09-16 10:07:29 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,10 +11,9 @@ int _isupper(int c)
 	    char c;
 
 	        c = 'C';
-		    printf("Return value when uppercase character %c is passed to isupper(): %d", c, isupper(c));
+		    return 1;
 
-		        c = '+';
-			    printf("\nReturn value when another character %c is passed to is isupper(): %d", c, isupper(c));
+		        c = 'c';
 
 			       return 0;
 }
