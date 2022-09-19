a9d36cc04ad29ef102e4a12b3bdbf6fad785a20c
Obadia <kipz.obed1@gmail.com>
2022-09-19 09:19:12 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,10 +9,11 @@ int _strlen(char *s)
 		int longi = 0;
 
 			while (*s != '\0')
-					{
-								longi++;
-										s++;
-											}
+			{
+			longi++;
+			s++;
+			}
 
-				return (longi);
+			return (longi);
+
 }
