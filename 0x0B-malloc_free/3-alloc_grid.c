4c3de56e01ef8b22b37c0072a32d782599a05b03
Obadia <kipz.obed1@gmail.com>
2022-10-04 11:11:03 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 			if (mee[x] == NULL)
 			{
 				for (; x >= 0; x--)
-				free(mee[x]);
+					free(mee[x]);
 
 				free(mee);
 				return (NULL);
@@ -35,7 +35,7 @@ int **alloc_grid(int width, int height)
 		for (x = 0; x < height; x++)
 		{
 			for (y = 0; y < width; y++)
-			mee[x][y] = 0;
+				mee[x][y] = 0;
 		}
 
 		return (mee);
